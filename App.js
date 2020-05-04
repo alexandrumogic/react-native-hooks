d43b9ae7838dbb746ab2d0b19b9c73ae86aa9cdf
Alexandru Mogic <alexandru.mogic@visma.com>
2020-05-04 17:27:29 +0300

    The code for useEffect leson

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  // const [state, setState] = useState(); 
+  const [task, setTask] = useState('Understand the useEffect');
+  const [taskList, setTaskList] = useState([]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {taskList.map((item) => {
+        return <Text>{item}</Text>
+      })}
+      <Text>You have to: {task}</Text>
+      <TextInput
+        style={{ borderWidth: 0.5, borderColor: 'gray', width: '50%', marginTop: 10 }}
+        onChangeText={(value) => { setTask(value) }}
+      />
+      <Button
+        title={"Add task"}
+        onPress={() => {
+          setTaskList([...taskList, task])
+        }}
+      />
     </View>
   );
 }
