75706920bc3659d56ae62cfd631a44807d4f62e2
Alexandru Mogic <alexandru.mogic@visma.com>
2020-05-14 19:35:58 +0300

    useRef: Accessing React components

--- TaskInput.js
+++ TaskInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useContext, useCallback, useMemo, useRef } from 'react';
 import { View, TextInput, Button, Text } from 'react-native';
 import { TaskListContext } from './App';
 
@@ -9,6 +9,8 @@ export default function TaskInput() {
   const [task, setTask] = useState('Understand the useEffect');
   const [state, dispatch] = useContext(TaskListContext);
   const [warning, setWarning] = useState(false);
+
+  const textInputRef = useRef();
 
   const callMeCallback = useCallback(() => {
 
@@ -27,12 +29,6 @@ export default function TaskInput() {
   set.add(callMe);
 
   // console.log(set);
-
-  const onTaskListChange = useCallback(() => {
-    setTask('');
-  }, [state])
-
-  useEffect(onTaskListChange, [state.taskList])
 
   const onTaskChange = useCallback(() => {
     console.log("onTaskChange");
@@ -54,14 +50,15 @@ export default function TaskInput() {
     return (
         <View style={{ width: '100%', alignItems: 'center' }}>
             <TextInput
-                value={task}
                 style={{ borderWidth: 0.5, borderColor: 'gray', width: '50%', marginTop: 10 }}
                 onChangeText={onChangeText}
+                ref={textInputRef}
             />
             <Button
                 title={"Add task"}
                 onPress={() => {
                   dispatch({type: 'add', payload: task})
+                  textInputRef.current.clear();
                 }}
             />
             {warning && <Text style={{ color: 'orange' }}>You have exceeded the limit</Text>}
